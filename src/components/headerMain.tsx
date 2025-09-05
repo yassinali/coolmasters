ad22f6aa657f4d80f4f11a1876996bbf9d886e60
yassinali <mozsoftware@gmail.com>
2025-09-05 23:46:11 +0200

    updated

--- src/components/headerMain.tsx
+++ src/components/headerMain.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import CartIcon from "./cartIcon";
 import Container from "./Container";
 import HeaderMenu from "./headerMenu";
 import Logo from "./logo";
 import MobileMenu from "./mobileMenu";
 import Searchbar from "./searchbar";
+import { getServerSession } from "@/lib/get-session";
 
 const Header = async () => {
+  const session = await getServerSession();
+  const user = session?.user;
+
   return (
     <header className="sticky top-0 z-50 bg-white/70 py-5 backdrop-blur-md">
       <Container className="text-lightColor flex items-center justify-between">
@@ -17,7 +22,7 @@ const Header = async () => {
         <div className="flex w-auto items-center justify-end gap-5 md:w-1/3">
           <Searchbar />
           <CartIcon />
-          Entrar
+          {user ? <p>{user.name}</p> : <Link href="/sign-in">Entrar</Link>}
         </div>
       </Container>
     </header>
